82d6e992a8eed2483405f1c373ef2d818186faa6
tsergium <tsergium@gmail.com>
2015-01-23 18:57:42 +0100

    Separated example csv test in it's own controller

---
+++ controller/exampleController.php
@@ -0,0 +1,28 @@
+<?php
+
+Class exampleController Extends baseController
+{
+    
+	public function index()
+	{
+		$addresses = array();
+		$file = new SplFileObject(__SITE_PATH . "/example.csv");
+		$file->setFlags(SplFileObject::READ_CSV);
+		$iterator = new LimitIterator($file, 1);
+
+		foreach ($iterator as $key => $row)
+		{
+			if(!empty($row[0]) && !empty($row[1])  && !empty($row[2]))
+			{
+				list($addresses[$key]['name'],$addresses[$key]['phone'],$addresses[$key]['street']) = $row;
+			}
+		}
+		if($_GET['id'] && array_key_exists($_GET['id'], $addresses))
+		{
+			$addresses = $addresses[$_GET['id']];
+		}
+
+		$this->registry->template->addresses = $addresses;
+		$this->registry->template->show('example');
+	}
+}
