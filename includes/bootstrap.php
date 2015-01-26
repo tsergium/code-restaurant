df014c18c07f686f3cceb2bd85ca89aaf30dade8
tsergium <tsergium@gmail.com>
2015-01-26 10:21:08 +0100

    Added autoloader inside bootstrap file

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,29 +1,26 @@
 <?php
+    ini_set('display_errors', 1);
     error_reporting(E_ALL);
     $sitePath = realpath(dirname(__FILE__)."/../");
     define ('__SITE_PATH', $sitePath);
 
     require_once __SITE_PATH . '/application/traits/' . 'request.traits.php';
-    require_once __SITE_PATH . '/application/' . 'controller_base.class.php';
-    require_once __SITE_PATH . '/application/' . 'registry.class.php';
-    require_once __SITE_PATH . '/application/' . 'router.class.php';
-    require_once __SITE_PATH . '/application/' . 'template.class.php';
-    require_once __SITE_PATH . '/application/' . 'db.class.php';
-
-    function __autoload($class_name)
+    
+    spl_autoload_register(function($class)
     {
-        $filename = strtolower($class_name) . '.class.php';
-        $file = __SITE_PATH . '/model/' . $filename;
-        if (file_exists($file) == false)
-        {
-            return false;
+        if(strpos($class, "Model_") !== false){
+            $filename = str_replace("Model_", "", $class . '.php');
+            $file = __SITE_PATH . '/model/' . $filename;
+        }else{
+             $file = __SITE_PATH . '/application/' . $class . '.php';
         }
-        include ($file);
-    }
+        include $file;
+    });
+    
 
     $registry = new Registry;
     $registry->db = Db::getInstance();
-    $registry->template = new template($registry);
+    $registry->template = new Template($registry);
     
     // set router
     $registry->router = new Router($registry);
