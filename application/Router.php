9c1c1858f9e7d30d897f8bd1f2b66382960621dd
tsergium <tsergium@gmail.com>
2015-02-04 17:03:53 +0100

    Code refactoring: Router Class

--- application/Router.php
+++ application/Router.php
@@ -23,15 +23,19 @@ class Router
 		$this->path = $path;
 	}
 
- 	public function loader()
- 	{
-		$this->getController();
-
+	private function checkIfReadable()
+	{
 		if(is_readable($this->file) == false)
 		{
 			$this->file = $this->path.'/errorController.php';
 			$this->controller = 'error';
 		}
+	}
+
+ 	public function loader()
+ 	{
+		$this->getController();
+		$this->checkIfReadable();
         
 		include $this->file;
 
@@ -49,18 +53,23 @@ class Router
 		$controller->$action();
  	}
 
+	private function parseRoute($route)
+	{
+		$parts = explode('/', $route);
+		$this->controller = $parts[0];
+		if(isset( $parts[1])){
+			$this->action = $parts[1];
+		}else{
+			$this->action = 'index';
+		}
+	}
+
 	private function getController()
 	{
 		$route = (empty($_GET['rt'])) ? '' : $_GET['rt'];
 		if(!empty($route) && $route != 'index.php')
 		{
-			$parts = explode('/', $route);
-			$this->controller = $parts[0];
-			if(isset( $parts[1])){
-				$this->action = $parts[1];
-			}else{
-                $this->action = 'index';
-            }
+			$this->parseRoute($route);
 		}
 
 		$this->file = $this->path .'/'. $this->controller . 'Controller.php';
