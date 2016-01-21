f85f85ec29f05c1da6af743a4e683109237b9f42
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2016-01-21 17:27:35 +0100

    General refactoring

--- controller/indexController.php
+++ controller/indexController.php
@@ -6,19 +6,4 @@ class indexController extends Controller
     {
         $this->registry->template->show('index-index');
     }
-
-    public function assignmentOne()
-    {
-        $this->registry->template->show('assignment1');
-    }
-
-    public function assignmentTwo()
-    {
-        $this->registry->template->show('assignment2');
-    }
-
-    public function assignmentThree()
-    {
-        $this->registry->template->show('assignment3');
-    }
 }
